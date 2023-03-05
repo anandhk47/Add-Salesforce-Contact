b7e502221b12f68bd639ef136b962db3fc54b1cb
anandhk47 <91628795+anandhk47@users.noreply.github.com>
2023-03-04 22:33:58 -0500

    Add files via upload

---
+++ MyCustomApp.js
@@ -0,0 +1,9 @@
+//import fetch from 'node-fetch';
+//globalThis.fetch = fetch;
+
+function getAccessToken() {
+    document.getElementsByTagName('p')[0].innerHTML = 'Test';
+
+}
+
+//getAccessToken();
