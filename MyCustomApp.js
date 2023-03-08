0819e144828b90a64301708c075905ea0bf642bc
anandhk47 <91628795+anandhk47@users.noreply.github.com>
2023-03-08 12:47:14 -0500

    Update MyCustomApp.js

--- MyCustomApp.js
+++ MyCustomApp.js
@@ -6,6 +6,25 @@ function getAuthCode() {
 
 }
 
+function getAccessToken() {
+    const headers = new Headers();
+    headers.append('Content-type','application/x-www-form-urlencoded');
+    headers.append('grant_type','authorization_code');    
+    headers.append('client_id','3MVG9riCAn8HHkYUvpxb2KieHak3WbYV1mLuu7Ye6m6ztSWhcuJfOssAGrfy9anoOzbutLarfaiEc.U09zO8S');
+    headers.append('client_secret','5B312EC1EC9524C082BF5CA34F750B3C39B447BAD3B1A09F14149DA5D7999BED');
+    headers.append('code',this.authCode);
+    headers.append('redirect_uri','https://anandhk47.github.io/Add-Salesforce-Contact/MyCustomApp.html');
+    const init = {
+        method: 'POST',
+        headers
+    }
+
+    fetch('https://cunning-fox-2chvvj-dev-ed.my.salesforce.com/services/oauth2/token', init)
+    .then(response => response.json())
+    .then(data => document.getElementsByTagName('p')[1].innerHTML = data.access_token  )
+    .catch(error => document.getElementsByTagName('p')[1].innerHTML = error);
+}
+
 const query = window.location.search;
 console.log('Query: '+query)
 var params = null
@@ -16,9 +35,6 @@ if (query != null) {
 }
 
 //document.getElementsByTagName('p')[0].innerHTML = 'Test ' ;
-if (authCode == null) {
-    getAuthCode();
-}
-else {
+if (authCode != null) {    
     document.getElementsByTagName('p')[0].innerHTML = authCode ;
 }
