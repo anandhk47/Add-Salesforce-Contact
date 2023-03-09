3b144584eef5f63ca7c851379d7e14431b112f4d
anandhk47 <91628795+anandhk47@users.noreply.github.com>
2023-03-08 21:57:30 -0500

    Update MyCustomApp.js

--- MyCustomApp.js
+++ MyCustomApp.js
@@ -9,18 +9,23 @@ function getAuthCode() {
 function getAccessToken() {
     const headers = new Headers();
     headers.append('Content-type','application/x-www-form-urlencoded');
-    headers.append('grant_type','authorization_code');    
-    headers.append('client_id','3MVG9riCAn8HHkYUvpxb2KieHak3WbYV1mLuu7Ye6m6ztSWhcuJfOssAGrfy9anoOzbutLarfaiEc.U09zO8S');
-    headers.append('client_secret','5B312EC1EC9524C082BF5CA34F750B3C39B447BAD3B1A09F14149DA5D7999BED');
-    headers.append('code',this.authCode);
-    headers.append('redirect_uri','https://anandhk47.github.io/Add-Salesforce-Contact/MyCustomApp.html');
+    //headers.append('grant_type','authorization_code');    
+    //headers.append('client_id','3MVG9riCAn8HHkYUvpxb2KieHak3WbYV1mLuu7Ye6m6ztSWhcuJfOssAGrfy9anoOzbutLarfaiEc.U09zO8S');
+    //headers.append('client_secret','5B312EC1EC9524C082BF5CA34F750B3C39B447BAD3B1A09F14149DA5D7999BED');
+    //headers.append('code',this.authCode);
+    //headers.append('redirect_uri','https://anandhk47.github.io/Add-Salesforce-Contact/MyCustomApp.html');
+    let query = 'grant_type=authorization_code&';
+    query+='client_id='+'3MVG9riCAn8HHkYUvpxb2KieHak3WbYV1mLuu7Ye6m6ztSWhcuJfOssAGrfy9anoOzbutLarfaiEc.U09zO8S';
+    query+='&client_secret='+'5B312EC1EC9524C082BF5CA34F750B3C39B447BAD3B1A09F14149DA5D7999BED';
+    query+='&code='+this.authCode;
+    query+='&redirect_uri='+'https://anandhk47.github.io/Add-Salesforce-Contact/MyCustomApp.html';
     const init = {
         method: 'POST',
         mode: 'no-cors',
         headers
     }
 
-    fetch('https://cunning-fox-2chvvj-dev-ed.my.salesforce.com/services/oauth2/token', init)
+    fetch('https://login.salesforce.com/services/oauth2/token?'+query, init)
     .then(response => response.json())
     .then(data => document.getElementsByTagName('p')[1].innerHTML = data.access_token  )
     .catch(error => document.getElementsByTagName('p')[1].innerHTML = error);
