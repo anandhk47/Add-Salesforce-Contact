c2c4a116b100e86a47b24a67b6f788951d405a18
anandhk47 <91628795+anandhk47@users.noreply.github.com>
2023-03-08 22:46:18 -0500

    Update MyCustomApp.js

--- MyCustomApp.js
+++ MyCustomApp.js
@@ -7,8 +7,8 @@ function getAuthCode() {
 }
 
 function getAccessToken() {
-    const headers = new Headers();
-    headers.append('Content-type','application/x-www-form-urlencoded');
+    //const headers = new Headers();
+    //headers.append('Content-type','application/x-www-form-urlencoded');
     //headers.append('grant_type','authorization_code');    
     //headers.append('client_id','3MVG9riCAn8HHkYUvpxb2KieHak3WbYV1mLuu7Ye6m6ztSWhcuJfOssAGrfy9anoOzbutLarfaiEc.U09zO8S');
     //headers.append('client_secret','5B312EC1EC9524C082BF5CA34F750B3C39B447BAD3B1A09F14149DA5D7999BED');
@@ -22,13 +22,13 @@ function getAccessToken() {
     const init = {
         method: 'POST',
         mode: 'no-cors',
-        credentials: 'include',
-        headers
+        credentials: 'include'
+        
     }
 
     fetch('https://login.salesforce.com/services/oauth2/token?'+query, init)
     .then(response => response.json())
-    .then(data => document.getElementsByTagName('p')[1].innerHTML = data.access_token  )
+    .then(data => document.getElementsByTagName('p')[1].innerHTML = JSON.stringify(data)  )
     .catch(error => document.getElementsByTagName('p')[1].innerHTML = error);
 }
 
